0ebe2a49c495c92e513de99d2ac26d7eb6cc82c1
ahmadkh1299 <167013067+ahmadkh1299@users.noreply.github.com>
2025-06-07 17:10:01 +0300

    Player1+2 implementation

--- src/Player1.cpp
+++ src/Player1.cpp
@@ -1,5 +1,15 @@
-//
-// Created by 97250 on 5/28/2025.
-//
-
 #include "../include/Player1.h"
+
+Player1::Player1(int player_index, size_t x, size_t y, size_t max_steps, size_t num_shells)
+        : Player(player_index, x, y, max_steps, num_shells),
+          player_index_(player_index),
+          board_width_(x),
+          board_height_(y) {}
+
+void Player1::updateTankWithBattleInfo(TankAlgorithm& tank, SatelliteView& satellite_view) {
+    auto* algo = dynamic_cast<ZoneControlAlgo*>(&tank);
+    if (!algo) return;
+
+    MyBattleInfo info(satellite_view, player_index_, board_height_, board_width_, {0, 0});
+    algo->updateBattleInfo(info);
+}
--- src/Player2.cpp
+++ src/Player2.cpp
@@ -1,5 +1,15 @@
-//
-// Created by 97250 on 5/28/2025.
-//
-
 #include "../include/Player2.h"
+
+Player2::Player2(int player_index, size_t x, size_t y, size_t max_steps, size_t num_shells)
+        : Player(player_index, x, y, max_steps, num_shells),
+          player_index_(player_index),
+          board_width_(x),
+          board_height_(y) {}
+
+void Player2::updateTankWithBattleInfo(TankAlgorithm& tank, SatelliteView& satellite_view) {
+    auto* algo = dynamic_cast<HunterAlgo*>(&tank);
+    if (!algo) return;
+
+    MyBattleInfo info(satellite_view, player_index_, board_height_, board_width_, {0, 0});
+    algo->updateBattleInfo(info);
+}
