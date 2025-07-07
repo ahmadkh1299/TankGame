a7f5ef0136f563610811635552a27e8a7c5cc7f8
Yaar Koren <forestkoren@gmail.com>
2025-07-08 00:50:27 +0300

    deleted comment about get battle info
    it should not be in tank class, it is handled by game manager class

--- include/Tank.h
+++ include/Tank.h
@@ -1,15 +1,15 @@
-// Cleaned and integrated version of Tank and GameManager headers and implementation
-// with corrected syntax, typos, and consistent handling
+//
+// Created by Radiant Forest on 21/04/2025.
+//
 
 #ifndef TANK_H
 #define TANK_H
 
-#include <memory>
 #include "GameObject.h"
 #include "MovingGameObject.h"
 #include "Position.h"
 #include "Direction.h"
-#include "TankAlgorithm.h"
+#include "commom/TankAlgorithm.h"
 #include "common/ActionRequest.h"
 
 class Tank : public MovingGameObject {
@@ -20,23 +20,29 @@ public:
     static constexpr int AFTER_SHOOT_WAIT_TURNS = 4;
     static constexpr int MOVE_BACK_WAIT_TURNS = 2;
 
+
+
     // Getters
-    char getSymbol() const override;
+    char getSymbol() const override; //each Tank Object will have a different symbol - "1" or "2", by the player it belongs to
     int getPlayerId() const;
     int getId() const;
     int getShellsLeft() const;
-    bool getIsWaitingToMoveBack() const;
-    bool getIsWaitingAfterShoot() const;
+    bool getIsWaitingToMoveBack() const; //the game manager needs this info, cuz it manages the turns. and the turns loop is what changing it.
+    bool getIsWaitingAfterShoot() const; //dido
     bool getIsRightAfterMoveBack() const;
     int getWaitToMoveBackCounter() const;
     ActionRequest getNextAction() const;
     ActionRequest getLastAction() const;
-    std::unique_ptr<TankAlgorithm>& getAlgorithm();
-    bool getIstRequestedBattleInfo() const;
+    std::unique_ptr<TankAlgorithm> getAlgorithm() const;
+    bool getIstRequestedBattleInfo() const; //not needed?
 
     // Setters
+    //void setShellsLeft();
+    //void setWaitAfterShootCounter();
+    //void setWaitToMoveBackCounter();
     void setNextAction(ActionRequest action);
-    void setLastAction(ActionRequest action);
+    void setLextAction(ActionRequest action)
+
 
     // Actions
     bool moveForward() override;
@@ -49,9 +55,6 @@ public:
     bool rotateFourthRight();
     void doNothing();
 
-    void decrementWaitToMoveBackCounter();
-    void decrementWaitAfterShootCounter();
-
     // Actions utility functions
     void updateWaitToMoveBackCounter();
     void updateWaitAfterShootCounter();
@@ -59,28 +62,38 @@ public:
     void resetIsWaitingToMoveBack();
     void resetIsRightAfterMoveBack();
 
-    void destroy();
-    bool isDestroyed() const;
-    void setPosition(Position newPos);
-    Position getPosition() const;
-    Direction getDirection() const;
 
 private:
-    int playerId_;
-    int id_;
+    int playerId_ = 0;
+    int id_ = 0;
+
     std::unique_ptr<TankAlgorithm> algorithm_;
-    bool requestedBattleInfo_ = false;
+
+    bool requestedBattleInfo_ = false; //not sure if neede
+
+    //shells and shoot state
     int shellsLeft_ = SHELLS_NUMBER;
-    bool isWaitingAfterShoot_ = false;
+    bool isWaitingAfterShoot_ = false; // cool down after shoot
     int waitAfterShootCounter_ = 0;
+
+    //moving-back state
     bool isWaitingToMoveBack_ = false;
     int waitToMoveBackCounter_ = 0;
     bool isRightAfterMoveBack_ = false;
+
     ActionRequest nextAction_;
     ActionRequest lastAction_;
 
+    // private methods - actions utility functions
     void actualRotateEighthLeft();
     void actualRotateEighthRight();
 };
 
 #endif //TANK_H
+
+
+
+
+
+
+
